1230f04eb46e53724d52724f9a7fc83053237c87
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-10 13:06:51 -0600

    Update reactStrictMode to false in next.config.js

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -6,7 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
