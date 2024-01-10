fb92bad785c196ffd9aff18c51b5534a245203a2
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-10 13:24:24 -0600

    Refactor CCTVImage component and add image click event handler

--- nextjs/src/pages/components/CCTVImage.tsx
+++ nextjs/src/pages/components/CCTVImage.tsx
@@ -12,12 +12,30 @@ const CCTVImage: React.FC<CCTVImageProps> = ({ cameraId }) => {
     const cctv = 'https://cctv.austinmobility.io/image/' + cameraId + '.jpg';
 
     const coordinates = useCoordinateStore((state) => state.coordinates);
-    console.log("coordinates", coordinates);
     const addCoordinates = useCoordinateStore((state) => state.addCoordinates)
 
+    useEffect(() => {
+        console.log("coordinates: ", coordinates);
+    }, [coordinates]);
+
+    const handleImageClick = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+        const imgElement = event.target as HTMLImageElement;
+        const originalWidth = imgElement.naturalWidth;
+        const originalHeight = imgElement.naturalHeight;
+        const currentWidth = imgElement.width;
+        const currentHeight = imgElement.height;
+
+        const xRatio = originalWidth / currentWidth;
+        const yRatio = originalHeight / currentHeight;
+
+        const originalX = event.clientX * xRatio;
+        const originalY = event.clientY * yRatio;
+
+        console.log(`Original X: ${originalX}, Original Y: ${originalY}`);
+    };
 
     return (
-        <img src={cctv} style={{ width: '50vw', height: '100vh', objectFit: 'contain' }} />
+        <img src={cctv} style={{ width: '50vw', height: '100vh', objectFit: 'contain' }} onClick={handleImageClick} />
     );
 };
 
