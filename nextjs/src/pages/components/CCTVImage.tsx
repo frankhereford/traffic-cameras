c3cc3a0ee642d1292dbf5aa35b0677192b5176c4
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 09:55:43 -0600

    Refactor CCTVImage component

--- nextjs/src/pages/components/CCTVImage.tsx
+++ nextjs/src/pages/components/CCTVImage.tsx
@@ -1,42 +1,48 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { useEffect } from 'react';
-import useCoordinateStore from '../hooks/CoordinateStore';
+import React, { useEffect } from "react"
+import useCoordinateStore from "../hooks/CoordinateStore"
 
 interface CCTVImageProps {
-    cameraId: number;
+  cameraId: number
 }
 
 const CCTVImage: React.FC<CCTVImageProps> = ({ cameraId }) => {
-    const cctv = 'https://cctv.austinmobility.io/image/' + cameraId + '.jpg';
+  const cctv = "https://cctv.austinmobility.io/image/" + cameraId + ".jpg"
 
-    const coordinates = useCoordinateStore((state) => state.coordinates);
-    const addCoordinates = useCoordinateStore((state) => state.addCoordinates)
+  const coordinates = useCoordinateStore((state) => state.coordinates)
+  const addCoordinates = useCoordinateStore((state) => state.addCoordinates)
 
-    useEffect(() => {
-        console.log("coordinates: ", coordinates);
-    }, [coordinates]);
+  useEffect(() => {
+    console.log("coordinates: ", coordinates)
+  }, [coordinates])
 
-    const handleImageClick = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-        const imgElement = event.target as HTMLImageElement;
-        const originalWidth = imgElement.naturalWidth;
-        const originalHeight = imgElement.naturalHeight;
-        const currentWidth = imgElement.width;
-        const currentHeight = imgElement.height;
+  const handleImageClick = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>,
+  ) => {
+    const imgElement = event.target as HTMLImageElement
+    const originalWidth = imgElement.naturalWidth
+    const originalHeight = imgElement.naturalHeight
+    const currentWidth = imgElement.width
+    const currentHeight = imgElement.height
 
-        const xRatio = originalWidth / currentWidth;
-        const yRatio = originalHeight / currentHeight;
+    const xRatio = originalWidth / currentWidth
+    const yRatio = originalHeight / currentHeight
 
-        const originalX = event.clientX * xRatio;
-        const originalY = event.clientY * yRatio;
+    const originalX = event.clientX * xRatio
+    const originalY = event.clientY * yRatio
 
-        console.log(`Original X: ${originalX}, Original Y: ${originalY}`);
-    };
+    console.log(`Original X: ${originalX}, Original Y: ${originalY}`)
+  }
 
-    return (
-        <img src={cctv} style={{ width: '50vw', height: '100vh', objectFit: 'contain' }} onClick={handleImageClick} />
-    );
-};
+  return (
+    <img
+      src={cctv}
+      style={{ width: "50vw", height: "100vh", objectFit: "contain" }}
+      onClick={handleImageClick}
+    />
+  )
+}
 
-export default CCTVImage;
+export default CCTVImage
