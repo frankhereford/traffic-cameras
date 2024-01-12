a12e8c3b4c3f9eb31dce2923e861e9e4a2ffc308
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 12:06:24 -0600

    linting

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -1,5 +1,5 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
   content: ["./src/**/*.tsx"],
@@ -11,4 +11,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
