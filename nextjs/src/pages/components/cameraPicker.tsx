8a10d4eb04f279984e757726276b0d0075caddbc
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 21:26:36 -0600

    Shuffle cameraData array when value changes in CameraPicker component

--- nextjs/src/pages/components/cameraPicker.tsx
+++ nextjs/src/pages/components/cameraPicker.tsx
@@ -56,9 +56,17 @@ const CameraPicker: React.FC = ({}) => {
   useEffect(() => {
     if (value !== "") {
       setCamera(parseInt(value))
-      //console.log("value changed: ", value)
+      shuffleArray(cameraData)
+      setCameraData([...cameraData])
     }
   }, [value])
+
+  const shuffleArray = (array: Camera[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j]!, array[i]!]
+    }
+  }
 
   return (
     <>
