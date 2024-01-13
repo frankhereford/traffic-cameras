b5d6d80e433db7174399969b7e68ecb84fd7600f
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 22:46:19 -0600

    Add click event handler to CCTV camera image

--- nextjs/src/pages/components/cctvCamera.tsx
+++ nextjs/src/pages/components/cctvCamera.tsx
@@ -5,6 +5,19 @@ const CctvCamera: React.FC = ({}) => {
   const camera = useIntersectionStore((state) => state.camera)
   const url = `https://cctv.austinmobility.io/image/${camera}.jpg`
 
+  const handleClick = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>,
+  ) => {
+    const img = event.currentTarget
+    const rect = img.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    const xRatio = img.naturalWidth / img.width
+    const yRatio = img.naturalHeight / img.height
+    const nativeX = Math.floor(x * xRatio)
+    const nativeY = Math.floor(y * yRatio)
+    console.log(`Clicked at native coordinates: ${nativeX}, ${nativeY}`)
+  }
   return (
     <>
       {camera ? (
@@ -14,6 +27,7 @@ const CctvCamera: React.FC = ({}) => {
           width={1920}
           height={1080}
           alt="CCTV Camera"
+          onClick={handleClick}
         />
       ) : null}
     </>
