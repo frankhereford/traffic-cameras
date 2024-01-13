f25b93047d6887bcd3544568d5f10359a4958207
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 22:50:30 -0600

    Add CCTV camera component with marker

--- nextjs/src/pages/components/cctvCamera.tsx
+++ nextjs/src/pages/components/cctvCamera.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image"
 import useIntersectionStore from "~/pages/hooks/IntersectionStore"
+import { useState } from "react"
+import styles from "./CctvCamera.module.css"
 
 const CctvCamera: React.FC = ({}) => {
   const camera = useIntersectionStore((state) => state.camera)
   const url = `https://cctv.austinmobility.io/image/${camera}.jpg`
+
+  const [clickPosition, setClickPosition] = useState<{
+    x: number
+    y: number
+  } | null>(null)
 
   const handleClick = (
     event: React.MouseEvent<HTMLImageElement, MouseEvent>,
@@ -17,18 +24,31 @@ const CctvCamera: React.FC = ({}) => {
     const nativeX = Math.floor(x * xRatio)
     const nativeY = Math.floor(y * yRatio)
     console.log(`Clicked at native coordinates: ${nativeX}, ${nativeY}`)
+    setClickPosition({ x: nativeX, y: nativeY })
   }
+
   return (
     <>
       {camera ? (
-        <Image
-          priority
-          src={url}
-          width={1920}
-          height={1080}
-          alt="CCTV Camera"
-          onClick={handleClick}
-        />
+        <div className={styles.relative}>
+          <Image
+            priority
+            src={url}
+            width={1920}
+            height={1080}
+            alt="CCTV Camera"
+            onClick={handleClick}
+          />
+          {clickPosition && (
+            <div
+              className={styles.marker}
+              style={{
+                left: `${clickPosition.x}px`,
+                top: `${clickPosition.y}px`,
+              }}
+            />
+          )}
+        </div>
       ) : null}
     </>
   )
