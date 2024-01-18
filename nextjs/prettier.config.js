9ba71f2e9eb90d8f1fe7a43b76e6514b8aa4071d
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-17 18:50:59 -0600

    tabula rasa

--- nextjs/prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  semi: false,
-  tabWidth: 2,
-};
-
-export default config;
