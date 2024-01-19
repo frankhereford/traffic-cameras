c2f74d88366e4fc877b546678b5a8fe613f05044
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-19 10:47:25 -0600

    Add Image component to display CCTV camera image

--- nextjs/src/pages/components/camera.tsx
+++ nextjs/src/pages/components/camera.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import useApplicationStore from "~/pages/hooks/applicationstore";
 
 function Camera() {
   const camera = useApplicationStore((state) => state.camera);
+  const url = `https://cctv.austinmobility.io/image/${camera}.jpg`;
   return (
     <>
-      <div>{camera}</div>
+      <Image
+        key={camera}
+        priority
+        src={`${url}?${new Date().getTime()}`}
+        width={1920}
+        height={1080}
+        alt="CCTV Camera"
+        // onClick={handleClick}
+        // onLoad={handleImageLoad}
+      />
     </>
   );
 }
