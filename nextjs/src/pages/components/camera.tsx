c2864802487ccb401239af874b4bf19e05420ccb
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-19 15:54:21 -0600

    Refactor Camera component to force re-render every 5 minutes

--- nextjs/src/pages/components/camera.tsx
+++ nextjs/src/pages/components/camera.tsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import useApplicationStore from "~/pages/hooks/applicationstore";
 
 function Camera() {
   const camera = useApplicationStore((state) => state.camera);
+  const [imageKey, setImageKey] = useState(Date.now());
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        setImageKey(Date.now()); // Change key to force re-render
+      },
+      5 * 60 * 1000,
+    ); // 5 minutes
+
+    return () => clearTimeout(timer); // Clear timeout if the component is unmounted
+  }, [imageKey]);
+
   const url = `https://cctv.austinmobility.io/image/${camera}.jpg`;
+
   return (
     <>
       {camera && (
         <Image
-          key={camera}
+          key={imageKey}
           priority
           src={`${url}?${new Date().getTime()}`}
           width={1920}
