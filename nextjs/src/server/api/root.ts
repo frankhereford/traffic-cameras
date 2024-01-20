9ae3bf0e30a6ad2a25f0bc33815b1bef435cc227
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-19 19:04:07 -0600

    Replace postRouter with cameraRouter in appRouter

--- nextjs/src/server/api/root.ts
+++ nextjs/src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { cameraRouter } from "~/server/api/routers/camera";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  camera: cameraRouter,
 });
 
 // export type definition of API
