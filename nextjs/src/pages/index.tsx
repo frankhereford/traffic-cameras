0f1cb7eaeac3a5d5544b55d0887fc0ec7ef58965
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-22 16:09:44 -0600

    Add fontsource package and import Roboto font styles

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
 import Head from "next/head"
 import CameraGeoreferenceApp from "~/pages/components/Application/CameraGeoreferenceApp"
 
