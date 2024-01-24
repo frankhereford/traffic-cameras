e12de44b58417bb32b3e91321aeb5d52c9d0cb3d
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-24 08:40:27 -0600

    Add status route for weather information retrieval

--- flask/app.py
+++ flask/app.py
@@ -16,6 +16,8 @@ import redis
 import pickle
 from xml.dom.minidom import parseString
 
+from routes.status import status
+
 
 # from queries import *
 
@@ -39,26 +41,8 @@ model = DetrForObjectDetection.from_pretrained(
 
 
 @app.route("/")
-def status():
-    # Get the current time
-    current_time = datetime.datetime.now().isoformat()
-
-    # Fetch weather data from NOAA API for Austin, TX (using station KAUS)
-    response = requests.get("https://api.weather.gov/stations/KAUS/observations/latest")
-    weather_data = response.json()
-
-    # Get weather description and corresponding emoji
-    weather_description = weather_data["properties"]["textDescription"]
-
-    # Return the current time and brief weather description
-    return jsonify(
-        {
-            "status": "nominal",
-            "description": "traffic-cameras backend python processor",
-            "current_time": current_time,
-            "current_weather_in_austin": weather_description,
-        }
-    )
+def status_route():
+    return status()
 
 
 @app.route("/image/<int:id>", methods=["GET"])
