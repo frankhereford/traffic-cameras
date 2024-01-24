bda29416a6cf378f9df7312ee50a8d94ea011459
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-24 13:58:32 -0600

    Refactor image processing and add throttling

--- python/routes/vision.py
+++ python/routes/vision.py
@@ -6,11 +6,10 @@ from PIL import Image
 import base64
 import tempfile
 from io import BytesIO
-from PIL import Image
 import time
 
-# import redis
-# import pickle
+import redis
+import pickle
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,15 +22,99 @@ model = DetrForObjectDetection.from_pretrained(
 )
 
 
+def throttle(func):
+    throttle._last_called = None
+
+    def wrapper(*args, **kwargs):
+        if throttle._last_called is not None:
+            time_since_last_call = time.time() - throttle._last_called
+            sleep_time = int(round(max(0, 3 - time_since_last_call), 0))
+            print("sleep time: ", sleep_time)
+            time.sleep(sleep_time)
+        throttle._last_called = time.time()
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 def vision_request_processor(db, redis):
     while True:
-        job = db.image.find_first(
+        process_one_image(db, redis)
+
+
+@throttle
+def process_one_image(db, redis):
+    job = db.image.find_first(
+        where={
+            "detectionsProcessed": False,
+        },
+    )
+    if job is None:
+        return
+
+    print(job.hash)
+
+    key = f"images:{job.hash}"
+
+    if redis.exists(key):
+        serialized_data = redis.get(key)
+        image_byte_stream = pickle.loads(serialized_data)
+    else:
+        db.image.update(
             where={
-                "detectionsProcessed": {"is": False},
+                "id": job.id,
+            },
+            data={
+                "detectionsProcessed": True,
             },
         )
-        print(job)
-        time.sleep(10)
+        return
+
+    image = Image.open(image_byte_stream)
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(
+        outputs, target_sizes=target_sizes, threshold=0.9
+    )[0]
+
+    detected_objects = []
+
+    for score, label, box in zip(
+        results["scores"], results["labels"], results["boxes"]
+    ):
+        box = [round(i, 2) for i in box.tolist()]
+        logging.info(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+        )
+
+        print("box: ", box)
+
+        # box:  [602.06, 217.31, 623.63, 260.28]
+        db.detection.create(
+            data={
+                "label": model.config.id2label[label.item()],
+                "confidence": round(score.item(), 3),
+                "xMin": box[0],
+                "yMin": box[1],
+                "xMax": box[2],
+                "yMax": box[3],
+                "imageId": job.id,
+            }
+        )
+
+    db.image.update(
+        where={
+            "id": job.id,
+        },
+        data={
+            "detectionsProcessed": True,
+        },
+    )
 
 
 # this is going to changed to just return what's in the DB
