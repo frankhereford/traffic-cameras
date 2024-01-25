6ecfbcd96998e42c669a37c2bef7cf57426574ce
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-24 20:08:39 -0600

    Add react-draggable library and implement Toolbar component

--- nextjs/src/pages/components/UI/Toolbar.tsx
+++ nextjs/src/pages/components/UI/Toolbar.tsx
@@ -1,7 +1,19 @@
-export default function Toolbar() {
+// import Auth from "./auth"
+// import CameraPicker from "./camerapicker"
+// import PickedPoints from "./pickedpoints"
+import Draggable from "react-draggable"
+
+export default function ToolPanel() {
   return (
     <>
-      <></>
+      <Draggable>
+        <div className="top-25 right-25 absolute z-50 w-[480px] rounded-lg bg-stone-50 pb-6 pl-[20px] pt-6">
+          <div>hello</div>
+          {/* <PickedPoints /> */}
+          {/* <CameraPicker /> */}
+          {/* <Auth /> */}
+        </div>
+      </Draggable>
     </>
   )
 }
