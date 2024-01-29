94802df04751c14ae545c74da96a1ffe6207677c
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-28 20:30:38 -0600

    Add getHistoricCameraDetections API endpoint

--- nextjs/src/server/api/routers/detection.ts
+++ nextjs/src/server/api/routers/detection.ts
@@ -20,4 +20,17 @@ export const detectionRouter = createTRPCRouter({
       })
       return image
     }),
+  getHistoricCameraDetections: protectedProcedure
+    .input(z.object({ camera: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const camera = await ctx.db.camera.findFirstOrThrow({
+        where: { coaId: input.camera },
+      })
+      const images = await ctx.db.image.findMany({
+        where: { cameraId: camera.id },
+        include: { detections: true },
+        orderBy: { createdAt: "desc" },
+      })
+      return images
+    }),
 })
