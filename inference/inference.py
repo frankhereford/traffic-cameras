e3eea252151fc2a3b4b9cf47bd2652b2ac3f23b3
Frank Hereford <frank@frankhereford.com>
2024-02-03 20:53:18 -0600

    Update docker-compose.yaml and add inference.py for streaming frames to RTMP

--- inference/inference.py
+++ inference/inference.py
@@ -47,38 +47,19 @@ os.makedirs(directory, exist_ok=True)
 hls_url = "http://10.10.10.97:8080/memfs/8bd9ac69-e88e-4f6c-a054-5a4176d597e3.m3u8"
 frame_generator = hls_frame_generator(hls_url)
 
-resolution_wy = (1920, 1080)
-thickness = sv.calculate_dynamic_line_thickness(resolution_wh=resolution_wy)
-
 model = get_roboflow_model("yolov8s-640")
 
-bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=4)
+byte_track = sv.ByteTrack(frame_rate=30)
 
-# frame_number = 0
-# start_time = time.time()
-
-# for frame in frame_generator:
-#     current_time = time.time()
-#     elapsed_time = current_time - start_time
-#     fps = frame_number / elapsed_time if elapsed_time > 0 else 0
-#     print("Frame: ", frame_number, "FPS: ", fps)
-#     frame_number += 1
-
-#     result = model.infer(frame)[0]
-#     detections = sv.Detections.from_inference(result)
-
-#     annotated_frame = frame.copy()
-#     annotated_frame = bounding_box_annotator.annotate(
-#         scene=annotated_frame, detections=detections
-#     )
-
-    # print("frame write out")
-    # cv2.imwrite(f"./frames/frame_{i}.jpg", annotated_frame)
+resolution_wy = (1920, 1080)
+thickness = sv.calculate_dynamic_line_thickness(resolution_wh=resolution_wy)
+text_scale = sv.calculate_dynamic_text_scale(resolution_wh=resolution_wy)
+bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=thickness)
+label_annotator = sv.LabelAnnotator(text_scale=text_scale, text_thickness=thickness)
 
 
 
 def stream_frames_to_rtmp(rtmp_url, frame_generator):
-    # FFmpeg command for streaming to RTMP
     command = (
         ffmpeg
         .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='1920x1080', framerate=30)  # Set input specifications
@@ -87,25 +68,29 @@ def stream_frames_to_rtmp(rtmp_url, frame_generator):
         .compile()
     )
 
-    # Start the FFmpeg process for streaming
     process = subprocess.Popen(command, stdin=subprocess.PIPE)
 
     for frame in frame_generator:
-        # Process the frame (your existing processing logic)
-        # ...
-
         result = model.infer(frame)[0]
         detections = sv.Detections.from_inference(result)
+        detections = byte_track.update_with_detections(detections)
+
+        labels = [
+            f"#{tracker_id}"
+            for tracker_id
+            in detections.tracker_id
+        ]
 
         annotated_frame = frame.copy()
         annotated_frame = bounding_box_annotator.annotate(
             scene=annotated_frame, detections=detections
         )
+        annotated_frame = label_annotator.annotate(
+            scene=annotated_frame, detections=detections, labels=labels
+        )
 
-        # Write the annotated frame to FFmpeg's stdin
         process.stdin.write(annotated_frame.tobytes())
 
-    # Close the FFmpeg process
     process.stdin.close()
     process.wait()
 
