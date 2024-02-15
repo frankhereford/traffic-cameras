8a00d76ba98e427a5a66017214ab1ce5a2d16105
Frank Hereford <github@frankhereford.com>
2024-02-15 18:14:36 +0000

    Update YouTube video ID in download_and_serve.py

--- live-traffic-analysis/download/download_and_serve.py
+++ live-traffic-analysis/download/download_and_serve.py
@@ -6,7 +6,7 @@ import subprocess
 
 def stream_video(video_id):
     youtube_url = f"https://www.youtube.com/watch?v={video_id}"
-    command = f"yt-dlp -f best {youtube_url} -o - | ffmpeg -stream_loop -1 -re -i pipe:0 -c:v libx264 -c:a aac -ar 44100 -strict experimental -f flv rtmp://10.0.3.228/9ea806cb-a214-4971-8b29-76cc9fc9de75.stream" 
+    command = f"yt-dlp -f best {youtube_url} -o - | ffmpeg -stream_loop -1 -re -i pipe:0 -c:v libx264 -c:a aac -ar 44100 -strict experimental -f flv rtmp://10.0.3.228/ccbb14ad-3d69-43d3-a6a0-5f34a23c2ef9.stream"
     subprocess.run(command, shell=True)
 
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         description="Stream a YouTube video to an RTSP server."
     )
     parser.add_argument(
-        "video_id", type=str, help="The ID of the YouTube video.", default="B0YjuKbVZ5w"
+        "video_id", type=str, help="The ID of the YouTube video.", default="ByED80IKdIU"
     )
 
     args = parser.parse_args()
