37cd3b003978869462306d2d1c821a65d20d211f
Frank Hereford <github@frankhereford.com>
2024-02-21 06:51:37 +0000

    Add script to reset new video queue in Redis

---
+++ live-traffic-analysis/inference/reset_new_video_queue.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import redis
+
+# Connect to Redis
+r = redis.Redis(host="localhost", port=6379, db=0)
+
+r.flushall()
+
+# List all .mp4 files in ./input_media
+files = [f for f in os.listdir("./input_media") if f.endswith(".mp4")]
+
+# Sort the files in alphabetical order
+files.sort()
+
+# Add the files to the Redis queue
+for file in files:
+    r.lpush("downloaded-videos-queue", file)
