f18823eae2a1ddca89d757970e2aa8c0ec0dba4e
Frank Hereford <github@frankhereford.com>
2024-02-21 00:50:55 +0000

    Add job processing and video information retrieval

--- live-traffic-analysis/inference/process_input_for_detections.py
+++ live-traffic-analysis/inference/process_input_for_detections.py
@@ -3,7 +3,9 @@
 import os
 import argparse
 import psycopg2
+from tqdm import tqdm
 import psycopg2.extras
+import supervision as sv
 import redis as redis_library
 from dotenv import load_dotenv
 
@@ -39,9 +41,39 @@ def receive_arguments():
     return args
 
 
+def get_a_job(redis):
+    _, value = redis.brpop("downloaded-videos-queue")
+    # Decode bytes object to string
+    value = value.decode("utf-8")
+    print("Received job: ", value)
+    return value
+
+
+def get_frame_generator(video):
+    video_path = f"./input_media/{video}"
+    frame_generator = sv.get_video_frames_generator(source_path=video_path)
+    return frame_generator
+
+
+def get_video_information(video):
+    video_path = f"./input_media/{video}"
+    information = sv.VideoInfo.from_video_path(video_path)
+    return information
+
+
 def main():
     db, redis = setup_service_handles()
     args = receive_arguments()
+    while True:
+        job = get_a_job(redis)
+        print(f"Processing video: {job}")
+        information = get_video_information(job)
+        print(f"Video information: {information}")
+        input = get_frame_generator(job)
+        for frame in tqdm(input, total=information.total_frames):
+            pass
+            # annotated_frame = self.process_frame(frame)
+            # sink.write_frame(annotated_frame)
 
 
 if __name__ == "__main__":
