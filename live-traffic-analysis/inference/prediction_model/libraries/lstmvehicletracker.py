2ea6fcf73aba0ebe920663930025dc3744e78d08
Frank Hereford <github@frankhereford.com>
2024-02-24 15:31:51 +0000

    Update LSTMVehicleTracker to support variable number of output pairs

--- live-traffic-analysis/inference/prediction_model/libraries/lstmvehicletracker.py
+++ live-traffic-analysis/inference/prediction_model/libraries/lstmvehicletracker.py
@@ -6,12 +6,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class LSTMVehicleTracker(nn.Module):
-    def __init__(self, input_size=2, hidden_size=128, num_layers=2, seq_length=30):
+    def __init__(
+        self, input_size=2, hidden_size=128, num_layers=2, seq_length=30, output_pairs=7
+    ):
         super(LSTMVehicleTracker, self).__init__()
         self.num_layers = num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.seq_length = seq_length
+        self.output_pairs = output_pairs
 
         self.lstm = nn.LSTM(
             input_size=input_size,
@@ -19,7 +22,9 @@ class LSTMVehicleTracker(nn.Module):
             num_layers=num_layers,
             batch_first=True,
         )
-        self.fc = nn.Linear(hidden_size, 12)  # Output is now 6 pairs of 2D (X,Y)
+        self.fc = nn.Linear(
+            hidden_size, (self.output_pairs * 2)
+        )  # Output is now self.output_pairs of 2D (X,Y)
 
     def forward(self, x):
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(
@@ -32,4 +37,6 @@ class LSTMVehicleTracker(nn.Module):
         output, (hn, cn) = self.lstm(x, (h_0, c_0))
         hn_last_layer = hn[-1, :, :]  # Get the hidden state of the last layer
         out = self.fc(hn_last_layer)
-        return out.view(-1, 6, 2)  # Reshape the output to have 6 pairs of (X,Y)
+        return out.view(
+            -1, self.output_pairs, 2
+        )  # Reshape the output to have 6 pairs of (X,Y)
